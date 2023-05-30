6c9781127629931e00027732e7ebc09d40c75844
Luca Ciraolo <lucaciraolo@gmail.com>
2023-05-30 09:21:05 +0100

    added min-version-finder

---
+++ apps/min-version-finder-e2e/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'min-version-finder-e2e',
+  preset: '../..//jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../..//coverage/min-version-finder-e2e',
+};
